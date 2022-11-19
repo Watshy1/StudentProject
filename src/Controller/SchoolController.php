ae6462c7149649b56fad343a21fed71b596827bd
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-11-19 17:24:50 +0100

    first commit

---
+++ src/Controller/SchoolController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\Persistence\ManagerRegistry;
+
+use App\Entity\School;
+use App\Repository\SchoolRepository;
+use App\Form\Type\SchoolType;
+
+class SchoolController extends AbstractController
+{
+    #[Route('/', name: 'app_school')]
+    public function index(SchoolRepository $schoolRepository): Response
+    {
+        $schools = $schoolRepository->findAll();
+
+        return $this->render('school/index.html.twig', [
+            'controller_name' => 'SchoolController',
+            'schools' => $schools,
+        ]);
+    }
+
+    #[Route('/school/show/{id}', name: 'app_school_show')]
+    public function show(School $school): Response
+    {
+        return $this->render('school/show.html.twig', [
+            'school' => $school,
+        ]);
+    }
+
+    #[Route('/school/create', name: 'app_school_create')]
+    public function new(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $form = $this->createForm(SchoolType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $school = new School();
+            $school->setName($form->get('name')->getData());
+
+            $entityManager->persist($school);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_school');
+        }
+
+        return $this->render('school/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/school/delete/{id}', name: 'app_school_delete')]
+    public function delete(School $school, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $entityManager->remove($school);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_school');
+    }
+}
---
+++ src/Form/Type/SchoolType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SchoolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, array('attr' => array(
+                'class' => 'mt-2 border text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white'
+            ),
+            'label_attr' => array(
+                'class' => 'block mb-2 text-sm font-medium text-white'
+            )))
+            ->add('create', SubmitType::class, array('attr' => array(
+                'class' => 'mt-2 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-6 py-2.5'
+            )))
+        ;
+    }
+}
